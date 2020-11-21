6586bbe697b51b222c9b7afb0b5a88c4fb93f038
abderrahim id boubker <idboubker98@gmail.com>
2020-11-21 15:06:58 +0100

    Last chages on this UI

--- lib/models/hotel_model.dart
+++ lib/models/hotel_model.dart
@@ -16,19 +16,19 @@ final List<Hotel> hotels = [
   Hotel(
     imageUrl: 'assets/images/hotel0.jpg',
     name: 'Hotel 0',
-    address: '404 Great St',
+    address: '123 Great St',
     price: 175,
   ),
   Hotel(
     imageUrl: 'assets/images/hotel1.jpg',
     name: 'Hotel 1',
-    address: '404 Great St',
+    address: '83 Great St',
     price: 300,
   ),
   Hotel(
     imageUrl: 'assets/images/hotel2.jpg',
     name: 'Hotel 2',
-    address: '404 Great St',
+    address: '207 Great St',
     price: 240,
   ),
 ];
